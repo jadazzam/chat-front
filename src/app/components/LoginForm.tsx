4c1fb0c0dfc34bbd19727df252c04408137c7d15
jazzam <jad.azzam@avec.fr>
2025-06-06 14:24:23 -0400

    Dissociate client & server parts of sign in form

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -1,29 +1,37 @@
+import React, {ReactNode} from "react"
 import {Button, TextField} from "@mui/material";
 import Form from "next/form";
 import {signIn} from "@/app/actions/login";
+import LoginFormWrapper from "@/app/components/Wrapper";
 
 export const LoginForm = () => {
     return (
-        <>
-            <Form action={async (formData) => await  signIn(formData)}>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Required"
-                    name="email"
-                    placeholder="john@example.com"
-                />
+        <LoginFormWrapper>
+            <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                name="email"
+                placeholder="john@example.com"
+            />
 
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="password"
-                    name='password'
-                    placeholder="password"
-                />
-                <Button type="submit" variant="contained">Sign in</Button>
-            </Form>
-        </>
+            <TextField
+                required
+                id="outlined-required"
+                label="password"
+                name='password'
+                placeholder="password"
+            />
+            <Button type="submit" variant="contained">Sign in</Button>
+        </LoginFormWrapper>
 
+    )
+}
+
+LoginForm.Wrapper = function LoginFormWrapper({children}: { children: ReactNode }) {
+    return (
+        <Form action={async (formData) => await signIn(formData)}>
+            {children}
+        </Form>
     )
 }
