b31e40ea1a2770cc694907ff95f7e44e53f279de
jazzam <jadazzam@me.com>
2025-06-12 17:43:38 -0400

    minor modif to trigger push

--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -3,9 +3,5 @@ import React from 'react';
 import RoomsList from '@/components/layouts/rooms';
 
 export default function Page() {
-  return (
-    <>
-      <RoomsList />
-    </>
-  );
+  return <RoomsList />;
 }
