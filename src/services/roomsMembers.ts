41a2f8ec4c5cd2aa7650f6af15893558179ed7ff
jazzam <jadazzam@me.com>
2025-06-16 19:38:15 -0400

    Put rooms members method

--- src/services/roomsMembers.ts
+++ src/services/roomsMembers.ts
@@ -17,3 +17,25 @@ export const postRoomMember = async ({ roomId, userId, token }: postRoomsMembers
     throw e;
   }
 };
+
+export const putRoomMember = async ({ roomId, userId, token }: postRoomsMembersType) => {
+  try {
+    const url = '/rooms-members/:roomId/:userId';
+    if (!roomId || !userId) new Error('roomId & userId must be provided');
+    if (roomId) url.replace(':roomId', roomId);
+    if (userId) url.replace(':userId', userId);
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + url, {
+      method: 'PUT',
+      headers: createHeaders(token),
+      body: JSON.stringify({ active: false }),
+    });
+    console.log('RESPONSE PUT =>', response);
+    if (!response.ok) {
+      new Error(response.statusText);
+    }
+    return response.json();
+  } catch (e) {
+    console.log('Error POST ', e);
+    throw e;
+  }
+};
