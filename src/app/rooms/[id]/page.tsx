1e9d33f43ba96928185f34685acd071e203eb291
jazzam <jadazzam@me.com>
2025-06-17 19:20:08 -0400

    Room page - socket : init, connect socket, join room, emit msg & receive msg from server

--- src/app/rooms/[id]/page.tsx
+++ src/app/rooms/[id]/page.tsx
@@ -1,25 +1,37 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { FormEvent, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { useAuth } from '@/providers/auth';
+import { useSocket } from '@/providers/socket';
+import { Button, TextField } from '@mui/material';
+import { createHeaders } from '@/services/headers';
+import { AuthContextType } from '@/types/auth';
 
 const Room = () => {
-  const auth = useAuth();
+  const auth: AuthContextType | null = useAuth();
+  const socket = useSocket().socket;
   const searchParams = useParams<{ id: string }>();
   const roomId = searchParams.id;
   const userId = auth?.user?.id;
-  console.log('searchParams', searchParams);
 
   useEffect(() => {
+    if (socket && roomId) {
+      // socket.on('connection', socket => {
+      socket.emit('join room', roomId);
+      // });
+      socket.on('joined room', joinedRoomId => {
+        console.log(`✅ Successfully joined room ${joinedRoomId}`);
+      });
+    }
     const handleBeforeUnload = () => {
       // Notify server that user is leaving
-      debugger;
-      navigator.sendBeacon('/api/leave-room', JSON.stringify({ roomId, userId }));
+      // debugger;
+      // navigator.sendBeacon('/api/leave-room', JSON.stringify({ roomId, userId }));
     };
 
     const handleVisibilityChange = () => {
       if (document.visibilityState === 'hidden') {
-        navigator.sendBeacon('/api/leave-room', JSON.stringify({ roomId, userId }));
+        // navigator.sendBeacon('/api/leave-room', JSON.stringify({ roomId, userId }));
       }
     };
 
@@ -30,9 +42,42 @@ const Room = () => {
       window.removeEventListener('beforeunload', handleBeforeUnload);
       document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
-  }, [roomId, userId]);
+  }, [roomId, userId, socket]);
 
-  return <h1>hello world</h1>;
+  useEffect(() => {
+    if (!socket) return;
+
+    const handleMessage = (data: { content: string; senderId?: string }) => {
+      console.log('📩 Received a message in room:', data.content);
+    };
+
+    socket.on('receive message', handleMessage);
+
+    return () => {
+      socket.off('receive message', handleMessage);
+    };
+  }, [socket]);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const content = formData.get('content');
+    const token = auth?.token ?? '';
+    const message = await fetch(process.env.NEXT_PUBLIC_API_URL + '/messages/', {
+      method: 'POST',
+      headers: createHeaders(token),
+      body: JSON.stringify({ content, room_id: roomId }),
+    });
+    if (message.ok && socket?.connected) {
+      socket.emit('send message', { roomId, content });
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField name="content" id="content" label="Type your message" variant="standard" />
+      <Button type="submit">Submit here</Button>
+    </form>
+  );
 };
 
 export default Room;
