aa0e7f91a4cfd92ba4e53cc061e0cee415ca4bb4
jazzam <jadazzam@me.com>
2025-06-19 16:03:45 -0400

    GET Method : fetch room by roomId with optional query complete=true

--- src/services/rooms.ts
+++ src/services/rooms.ts
@@ -1,8 +1,10 @@
 import { RoomsType } from '@/types/rooms';
+import { MessageApiType } from '@/types/messages';
+import { createHeaders } from '@/services/headers';
 
 export const getRooms = async (token: string): Promise<RoomsType[]> => {
   try {
-    const response = await fetch('http://localhost:8080/rooms', {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/rooms', {
       method: 'GET',
       headers: {
         Accept: 'application/json',
@@ -12,7 +14,34 @@ export const getRooms = async (token: string): Promise<RoomsType[]> => {
     });
     return await response.json();
   } catch (error) {
-    console.error('Error getRooms', error);
+    console.error('Error GET getRooms', error);
     return [];
+  }
+};
+
+export const getRoom = async ({
+  token,
+  roomId,
+  complete = false,
+}: {
+  token: string | null;
+  roomId: string;
+  complete: boolean;
+}): Promise<{ data: RoomsType[]; messages: MessageApiType[] }> => {
+  try {
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_API_URL + `/rooms/${roomId}?complete=${complete}`,
+      {
+        method: 'GET',
+        headers: createHeaders(token || undefined),
+      }
+    );
+    if (!res.ok) {
+      new Error(`Error getRoomMessages status: ${res.status}`);
+    }
+    return await res.json();
+  } catch (e) {
+    console.error('Error GET getRoomMessages by roomId', e);
+    throw e;
   }
 };
